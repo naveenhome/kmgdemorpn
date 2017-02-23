cfc7c2b9b9f42459f442a86ba3c176c6e3255b0f
Naveen Singh <naveen.singh@leanpitch.com>
2017-02-23 12:33:58 +0530

    Change in java file

--- src/main/java/rpn/calculator/MultiplyOperation.java
+++ src/main/java/rpn/calculator/MultiplyOperation.java
@@ -3,7 +3,6 @@ package rpn.calculator;
 public class MultiplyOperation implements BinaryOperation {
 
 	public int Calculate(int firstValue, int secondValue) {
-		// TODO Auto-generated method stub
 		return firstValue*secondValue;
 	}
 	
